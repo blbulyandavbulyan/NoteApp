1b13c90c73e0874eba2433a99fda9fdd85320e59
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-08 17:36:37 +0400

    Added NoteRepository

---
+++ NoteRepository.py
@@ -0,0 +1,27 @@
+from note import Note, BaseNote
+from abc import ABC, abstractmethod
+
+
+class AbstractNoteRepository(ABC):
+    def __init__(self, notes: list[Note]):
+        self.__notes: dict[int, Note] = {}
+        self.__next_id: int = notes[0].id
+        for note in notes:
+            if note.id not in notes:
+                self.__notes[note.id] = note
+                if note.id > self.__next_id:
+                    self.__next_id = note.id
+            else:
+                raise RuntimeError(f"Дублирующиеся ИД контактов {note.id}")
+
+    def __get_next_id(self) -> int:
+        result = self.__next_id
+        self.__next_id += 1
+        return result
+
+    def add(self, base_note: BaseNote):
+        new_id = self.__get_next_id()
+        self.__notes[new_id] = Note(new_id, base_note)
+
+    def __getitem__(self, item):
+        return self.__notes[item]
