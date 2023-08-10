fdc20066812ff968b4f5267120c9b6a14a7cb1fd
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-10 17:05:56 +0400

    moved save date to Note class

--- note.py
+++ note.py
@@ -2,10 +2,9 @@ from datetime import datetime
 
 
 class BaseNote(object):
-    def __init__(self, title: str, text: str, save_date: datetime):
+    def __init__(self, title: str, text: str):
         self.title = title
         self.text = text
-        self.save_date = save_date
 
     @property
     def title(self) -> str:
@@ -23,21 +22,18 @@ class BaseNote(object):
     def text(self, value: str):
         self.__text = value
 
-    @property
-    def save_date(self) -> datetime:
-        return self.__save_date
-
-    @save_date.setter
-    def save_date(self, value: datetime):
-        self.__save_date = value
-
 
 class Note(BaseNote):
 
-    def __init__(self, note_id: int, base_note: BaseNote):
-        super().__init__(base_note.title, base_note.text, base_note.save_date)
+    def __init__(self, note_id: int, save_date: datetime, base_note: BaseNote):
+        super().__init__(base_note.title, base_note.text)
         self.__note_id = note_id
+        self.__save_date = save_date
 
     @property
     def id(self) -> int:
         return self.__note_id
+
+    @property
+    def save_date(self) -> datetime:
+        return self.__save_date
