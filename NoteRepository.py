8740f0e3fb65be56577d9a9aafd56dab3981be2f
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-10 17:06:22 +0400

    added realization for saving and load in csv from csv

--- NoteRepository.py
+++ NoteRepository.py
@@ -1,15 +1,23 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from note import Note, BaseNote
+import csv
+from datetime import datetime
 
 
-class AbstractNoteRepository(ABC):
-    def __init__(self, notes: list[Note]):
-        self.__notes: dict[int, Note] = {}
+class AbstractNoteFileRepository(ABC):
+    def __init__(self, file_name: str):
+        self._notes: dict[int, Note] = {}
+        self.__file_name = file_name
+        self._datetime_format = '%m/%d/%y %H:%M:%S'
+        self.load(file_name)
+        self.__saved = True
+
+    def _parse_list_of_notes(self, notes: list[Note]):
         self.__next_id: int = notes[0].id
         for note in notes:
             if note.id not in notes:
-                self.__notes[note.id] = note
+                self._notes[note.id] = note
                 if note.id > self.__next_id:
                     self.__next_id = note.id
             else:
@@ -22,7 +30,47 @@ class AbstractNoteRepository(ABC):
 
     def add(self, base_note: BaseNote):
         new_id = self.__get_next_id()
-        self.__notes[new_id] = Note(new_id, base_note)
+        self._notes[new_id] = Note(new_id, base_note)
 
     def __getitem__(self, item):
-        return self.__notes[item]
+        return self._notes[item]
+
+    @property
+    def file_name(self):
+        return self.__file_name
+
+    @property
+    def saved(self):
+        return self.__saved
+
+    @abstractmethod
+    def load(self, file_name: str):
+        self.__file_name = file_name
+        self.__saved = True
+
+    @abstractmethod
+    def save(self):
+        self.__saved = True
+
+
+class CsvNoteFileRepository(AbstractNoteFileRepository):
+    def __init__(self, file_name: str):
+        super().__init__(file_name)
+
+    def load(self, file_name: str):
+        notes_list: list[Note] = []
+        with open(file_name, 'r', encoding="UTF-8") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                notes_list.append(
+                    Note(int(row[0]), datetime.strptime(row[1], self._datetime_format), BaseNote(row[2], row[3]))
+                )
+        super().load(file_name)
+        self._parse_list_of_notes(notes_list)
+
+    def save(self):
+        with open(self.file_name, 'w', encoding="UTF-8") as file:
+            writer = csv.writer(file)
+            for (_, note) in self._notes:
+                writer.writerow([note.id, note.save_date, note.title, note.text])
+        super().save()
